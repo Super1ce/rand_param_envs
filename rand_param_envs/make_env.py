d85adca6c1ac7b2da5d9a9b0c956addb082a3b1b
Super1ce <278042904@qq.com>
2024-01-10 09:46:31 +0800

    modify make_env

--- rand_param_envs/make_env.py
+++ rand_param_envs/make_env.py
@@ -1,16 +1,20 @@
 
 import os
 
-from rand_param_envs import gym
 import numpy as np
 import torch
 from gym.spaces.box import Box
 
 from .wrappers import VariBadWrapper
+from .hopper_rand_params import HopperRandParamsEnv
+from .walker2d_rand_params import Walker2DRandParamsEnv
 
 
 def make_env(env_id, episodes_per_task, seed=None, **kwargs):
-    env = gym.make(env_id, **kwargs)
+    if env_id == 'walker_params':
+        env = Walker2DRandParamsEnv(**kwargs)
+    elif env_id == 'hopper_params':
+        env = HopperRandParamsEnv(**kwargs)
     if seed is not None:
         env.seed(seed)
     env = VariBadWrapper(env=env,
